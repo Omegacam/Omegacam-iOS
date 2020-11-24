e6743d7296d5511ef4c1c6cc567d3e4feabf2b54
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2020-11-23 23:17:12 -0800

    Create ios project

---
+++ Omegacam-ios/mainView.swift
@@ -0,0 +1,19 @@
+//
+//  mainView.swift
+//  Omegacam-ios
+//
+//  Created by Richard Wei on 11/23/20.
+//
+
+import UIKit
+
+class mainView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
